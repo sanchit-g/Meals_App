2373944e450f0fff515d1274ae542b72040b2707
Sanchit Gupta <19ucc046@lnmiit.ac.in>
2020-11-02 21:09:07 +0530

    seventh commit

--- lib/screens/favorites_screen.dart
+++ lib/screens/favorites_screen.dart
@@ -4,7 +4,7 @@ class FavoritesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text('The Favorites is Ashi'),
+      child: Text('This feature is under development'),
     );
   }
 }
